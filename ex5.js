90e795c736ebc7a6283445390f5c56b5893c6bc3
omiras <oscar.miras@gmail.com>
2023-10-07 10:13:31 +0200

    done

--- ex5.js
+++ ex5.js
@@ -1,11 +1,18 @@
 /**
  * Crea un código de manera que, al ejecutar node ex5.js, pase lo siguiente:
- * 
+ *
  * 1. Si la variable número vale 5 o más, mostramos por el terminal "El número es mayor de 5"
  * 2. En caso contrario, mostramos por el temrinal "El número es menor de 5"
+ *
+ * Vídeo: https://oscarm.tinytake.com/msc/ODc3NTg0NV8yMjE5Mjc5NQ
  */
 
-let numero = 5;
+let numero = 1;
 
 // AQUÍ TU CÓDIGO. Necesitas codificar una estructura if...else. Mira los ejemplos anteriores. Cambia el valor de la variable "numero" para comprobar que has implementado bien la solución
 
+if (numero >= 5) {
+  console.log("El número es mayor o igual a 5.");
+} else {
+  console.log("El número es menor a 5.");
+}

dd9cb3a86d76632714d2185618d331ef5bd38e67
omiras <oscar.miras@gmail.com>
2023-10-07 10:02:41 +0200

    ex4 fix

--- ex4.js
+++ ex4.js
@@ -3,12 +3,16 @@
  * 2. Asigna un valor adecuado a la variable "número" para que, por consola, se muestra tan solo "BUP!BUP!"
  */
 
-let numero = -1;
+let numero = 0; /** Solamente puedes modificar esta línea */
 
-if (numero == 0) {
-    console.log("BUP!BUP!");
+if (numero <= 0) {
+  console.log("BUP!BUP!");
 }
 
 if (numero <= -1) {
-    console.log("CROAK!!");
+  console.log("CROAK!!");
 }
+
+/**
+ * Respuesta: El valor buscado es el 0. (0<=0) se evalúa como true, puesto que 0 es menor o igual a 0. Por otro lado (0<=-1) se evalúa como false, puesto que 0 NO es menor o igual a -1
+ */
